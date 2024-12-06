aab656b35fd162db9ca4d8efa40009aa6e883d75
enayetaitech <mdrehman@365aitech.com>
2024-12-06 12:57:46 +0600

    nav button active color done

--- frontend/src/components/dashboard/Sidebar.tsx
+++ frontend/src/components/dashboard/Sidebar.tsx
@@ -16,6 +16,7 @@ import { useSelector } from "react-redux";
 import { sidebarItems } from "@/constants";
 import { NavItem } from "@/types";
 import { Menu } from "lucide-react";
+import CustomButton from "../shared/CustomButton";
 
 const Sidebar = ({
   setActiveComponent,
@@ -23,6 +24,7 @@ const Sidebar = ({
   setActiveComponent: (component: React.ReactNode) => void;
 }) => {
   const [isSheetOpen, setIsSheetOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState<string | null>(null);
   const user = useSelector((state: RootState) => state.auth.user);
   const role = user?.role as "ADMIN" | "VENDOR" | "CUSTOMER";
   console.log(user);
@@ -35,17 +37,24 @@ const Sidebar = ({
       {/* Sidebar for Large Screens */}
       <div className="hidden lg:block fixed top-0 left-0 h-full w-[300px] shadow-lg bg-gradient-to-t from-primary to-accent dark:from-yellow-500 dark:to-yellow-600">
         <div className="p-4">
-          <h2 className="text-xl font-bold">Welcome {user?.name}</h2>
+          <h2 className="text-xl font-bold pb-5">Welcome {user?.name}</h2>
           {navItems.map((navItem: NavItem, index: number) => {
             console.log("navItem.component:", navItem.component);
             return (
-              <button
+              <CustomButton
                 key={index}
-                className="w-full text-left py-2 px-4 mb-2 rounded-md hover:bg-gray-200"
-                onClick={() => setActiveComponent(<navItem.component />)}
+                className={`w-full text-left py-3 px-4 mb-2 rounded-md ${
+                  activeItem === navItem.label
+                    ? "bg-primary text-white"
+                    : "bg-muted text-muted-foreground hover:bg-primary-foreground hover:text-white"
+                }`}
+                onClick={() => {
+                  setActiveItem(navItem.label); // Set active item
+                  setActiveComponent(<navItem.component />);
+                }}
               >
                 {navItem.label}
-              </button>
+              </CustomButton>
             );
           })}
         </div>
@@ -76,13 +85,20 @@ const Sidebar = ({
             <SheetTitle>Welcome {user?.name}</SheetTitle>
             <SheetDescription>
               {navItems.map((navItem: NavItem, index: number) => (
-                <button
+                <CustomButton
                   key={index}
-                  className="w-full text-left py-2 px-4 mb-2 rounded-md hover:bg-gray-200"
-                  onClick={() => setActiveComponent(<navItem.component />)}
+                  className={`w-full text-left py-3 px-4 mb-2 rounded-md ${
+                    activeItem === navItem.label
+                      ? "bg-primary text-white"
+                      : "bg-muted text-muted-foreground hover:bg-primary-foreground hover:text-white"
+                  }`}
+                  onClick={() => {
+                    setActiveItem(navItem.label); // Set active item
+                    setActiveComponent(<navItem.component />);
+                  }}
                 >
                   {navItem.label}
-                </button>
+                </CustomButton>
               ))}
             </SheetDescription>
           </SheetHeader>
