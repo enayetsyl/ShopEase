12e135840d8be3bbbd21687cda3e72885001c1bf
enayetaitech <mdrehman@365aitech.com>
2024-12-08 14:23:28 +0600

    heading added in the product component

--- frontend/src/components/home/Products.tsx
+++ frontend/src/components/home/Products.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductCard from "../shared/ProductCard";
 import { useGetProductsQuery } from "@/redux/api/productApi";
+import Heading from "../shared/CustomHeading";
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
@@ -10,10 +11,17 @@ const Products = () => {
   }
 
   return (
-    <div className="flex flex-wrap justify-center gap-6 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
-      {data?.map((product) => (
-        <ProductCard key={product.productId} product={product} />
-      ))}
+    <div>
+      <Heading
+        text="Our Products"
+        className="text-4xl lg:text-6xl text-center pb-20"
+      />
+
+      <div className="flex flex-wrap justify-center gap-6 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+        {data?.map((product) => (
+          <ProductCard key={product.productId} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
