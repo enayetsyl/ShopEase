4728c19cb39bf84604fb2160b21f01ba40147b6d
enayetaitech <mdrehman@365aitech.com>
2024-12-10 18:23:24 +0600

    edit product form added

--- frontend/src/components/dashboard/admin/category/EditCategoryInfoModal.tsx
+++ frontend/src/components/dashboard/admin/category/EditCategoryInfoModal.tsx
@@ -11,15 +11,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-
-const editCategorySchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long."),
-  description: z
-    .string()
-    .min(10, "Description must be at least 10 characters long."),
-});
-
-type EditCategoryFormValues = z.infer<typeof editCategorySchema>;
+import EditCategoryForm, { EditCategoryFormValues } from "@/components/forms/EditCategoryForm";
 
 const EditCategoryInfoModal = ({
   onClose,
@@ -32,18 +24,6 @@ const EditCategoryInfoModal = ({
   onSubmit: (data: EditCategoryFormValues) => void;
   isLoading: boolean;
 }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<EditCategoryFormValues>({
-    resolver: zodResolver(editCategorySchema),
-    defaultValues: {
-      name: category.name,
-      description: category.description,
-    },
-  });
-
   return (
     <DialogContent
       className="text-card-foreground"
@@ -55,33 +35,11 @@ const EditCategoryInfoModal = ({
           Update the Category details below.
         </DialogDescription>
       </DialogHeader>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <CustomInput
-          placeholder="Category Name"
-          label="Name *"
-          inputClassName="bg-white py-4 focus-visible:ring-0 dark:text-black"
-          {...register("name")}
-          error={errors.name?.message}
-        />
-
-        <CustomInput
-          placeholder="Category Description"
-          label="Description *"
-          inputClassName="bg-white py-4 focus-visible:ring-0 dark:text-black"
-          {...register("description")}
-          error={errors.description?.message}
-        />
-
-        <div className="mt-4">
-          <CustomButton
-            type="submit"
-            className="bg-primary text-black"
-            loading={isLoading}
-          >
-            Update Category
-          </CustomButton>
-        </div>
-      </form>
+      <EditCategoryForm
+        category={category}
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+      />
     </DialogContent>
   );
 };
