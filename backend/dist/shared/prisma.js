14e1cb6100945b6fdd1def1c3eb21d7b9b9950fa
enayetaitech <mdrehman@365aitech.com>
2024-12-11 17:56:43 +0600

    prisma db deployed

--- backend/dist/shared/prisma.js
+++ backend/dist/shared/prisma.js
@@ -3,4 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 // console.log(prisma);
+prisma.$connect()
+    .then(() => console.log('Connected to database'))
+    .catch((err) => console.error('Failed to connect:', err));
 exports.default = prisma;
