2047c1d1ca0ab12db2f22dc9caa9ba47c337c259
enayetaitech <mdrehman@365aitech.com>
2024-12-11 20:01:16 +0600

    redux base url changed

--- frontend/src/redux/api/baseApi.ts
+++ frontend/src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shop-ease-lyart.vercel.app/api/v1",
+    baseUrl: "https://shopease-w422.onrender.com/api/v1",
     credentials: "include",
     prepareHeaders: (headers) => {
       const token = Cookies.get("accessToken");
