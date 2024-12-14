187d3e250b4d77a61aaa5683678c930a8ef80f12
enayetaitech <mdrehman@365aitech.com>
2024-12-14 18:24:43 +0600

    shop card skeleton added

--- frontend/src/app/(root)/shop/page.tsx
+++ frontend/src/app/(root)/shop/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Heading from "@/components/shared/CustomHeading";
 import { ShopCard } from "@/components/shop/ShopCard";
+import { ShopCardSkeleton } from "@/components/shop/ShopCardSkeleton";
 import { useGetAllShopQuery } from "@/redux/api/shopApi";
 import React from "react";
 
 const Shop = () => {
-  const { data } = useGetAllShopQuery({ page: 1, limit: 10 });
-  console.log('data', data)
+  const { data, isLoading } = useGetAllShopQuery({ page: 1, limit: 10 });
+  console.log("data", data);
 
   return (
     <>
@@ -17,8 +18,15 @@ const Shop = () => {
         />
       </div>
 
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {data && data?.data.map((shop) => <ShopCard key={shop.shopId} shop={shop} />)}
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-20">
+        {isLoading
+          ? Array.from({ length: 6 }).map((_, index) => (
+              <ShopCardSkeleton key={index} />
+            ))
+          : data &&
+            data?.data.map((shop) => (
+              <ShopCard key={shop.shopId} shop={shop} />
+            ))}
       </div>
     </>
   );
